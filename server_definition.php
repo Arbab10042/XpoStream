9b430cc4517d7539ed2c7666c4bd036cc19a44f0
Arbab10042 <arbabhamd@gmail.com>
2021-12-02 23:48:05 +0500

    init

---
+++ server_definition.php
@@ -0,0 +1,11 @@
+<?php
+define("SERVER_NAME", "localhost");
+define("DB_USERNAME", "animecorner_me");
+define("DB_PASSWORD", "<@AnimeCorner123>");
+define("DB_NAME", "xpostream");
+
+define("BASE_URL", "http://localhost/XpoStream/");
+define("LOGOUT_URL", BASE_URL."logout.php");
+
+define("SESS_KEY", "Portal");
+?>
---
+++ server_lock.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+if(!isset($_SESSION['user_id'])){
+    echo "<script>setTimeout(function(){ window.location = 'login.php'; }, 10);</script>";
+    die();
+}
+?>
